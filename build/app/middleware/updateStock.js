0dca17e96cfe92baa23b941ded171656b112431b
Thiago Santos <tsrocha901@gmail.com>
2020-06-04 19:55:02 -0300

    novo build do sucrase

--- build/app/middleware/updateStock.js
+++
@@ -1,7 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _StockController = require('../controllers/StockController'); var _StockController2 = _interopRequireDefault(_StockController);
-
-exports. default = function(req, res, next) {
-  next();
-
-  _StockController2.default.store(req, res);
-}
